7f179dfba42202cb689ee5017d22f98760833efb
Vyacheslav Kurbatov <Kurbatov-Slavard@yandex.ru>
2022-12-19 13:26:13 +0300

    Add task2.1 Home

---
+++ HomeWorkSem2/Task2_1.py
@@ -0,0 +1,33 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
+import math
+
+print('Программа считает сумму цифр в введенном числе')
+
+
+def input_data():
+    while True:
+        try:
+            num = float(input('Введите число: '))
+            if type(num) == float:
+                return num
+        except:
+            print('Вы ввели не число.')
+
+
+def sum_digits(num):
+    num = abs(num)
+    num_str = str(num)
+    num_str = num_str.replace('.', '')
+    result = 0
+    for i in range(0, len(num_str), 1):
+        num = int(num_str[i])
+        result += num
+    return result
+
+
+number = input_data()
+res = sum_digits(number)
+print(f'Сумма цифр в числе {number} = {res}')
